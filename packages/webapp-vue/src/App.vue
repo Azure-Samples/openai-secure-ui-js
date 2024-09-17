cff3f53c21f7dfdd19dc11bf1f6bda3ce49daec2
sinedied <noda@free.fr>
2024-09-17 15:06:43 +0200

    feat: update vue integration

--- packages/webapp-vue/src/App.vue
+++ packages/webapp-vue/src/App.vue
@@ -3,8 +3,19 @@ import '@azure/ai-chat-components';
 </script>
 
 <template>
-  <nav>AI Chat</nav>
-  <azc-chat></azc-chat>
+  <nav>
+    <div class="container">
+      <img class="logo" src="../favicon.png" alt="AI Chat logo" width="36" height="36" />
+      AI Chat
+      <div class="spacer"></div>
+      <azc-auth type="status"></azc-auth>
+    </div>
+  </nav>
+  <div class="container">
+    <azc-auth>
+      <azc-chat></azc-chat>
+    </azc-auth>
+  </div>
 </template>
 
 <style scoped>
@@ -21,9 +32,22 @@ nav {
     sans-serif;
   font-size: 1.25rem;
 }
-azc-chat {
-  display: block;
+.container {
   max-width: 1024px;
   margin: 0 auto;
+  display: flex;
+  align-items: center;
+}
+.logo {
+  margin-right: 8px;
+}
+.spacer {
+  flex: 100 1 0;
+}
+azc-chat, azc-auth {
+  flex: auto;
+}
+azc-auth[type="status"] {
+  font-size: 16px;
 }
 </style>
